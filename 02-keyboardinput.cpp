a6fbead6c386302c759aa803d9cab132994e47e2
cha1ra <cha1ra.garnet@gmail.com>
2021-04-04 00:11:48 +0900

    変数の宣言

--- 02-keyboardinput.cpp
+++ 02-keyboardinput.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
 int main() {
+    int x, y;
+    // x, y に値を宣言しないと 何が代入されるわからない
+    x = 63;
+    y = 18;
+    std::cout << "x = " << x << std::endl;
+    std::cout << "y = " << y << std::endl;
     // 18や63は整数リテラルと呼ぶ
-    std::cout << "18+63=" << (18+63) << std::endl;
+    std::cout << "x+y=" << (x+y) << std::endl;
+    // int型で宣言しているので切り捨てられる
+    std::cout << "(x+y)/2=" << (x+y)/2 << std::endl;
 }
