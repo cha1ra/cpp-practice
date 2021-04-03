7e11abbc8ff406a55249f157e5e60a082f5a5e95
cha1ra <cha1ra.garnet@gmail.com>
2021-04-04 00:46:23 +0900

    ランダム数

--- 03-keyboardinput.cpp
+++ 03-keyboardinput.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <random>
 
 // google の c++ コーディング規約で 定数の前には k をつけようと言われている
 const double kTax = 0.8;
 
 int main()
 {
+    std::random_device seed_gen;
+    std::mt19937 engine(seed_gen);
+    std::uniform_int_distribution<> dist(0, 9);
+    int lucky = dist(engine);
+
+    std::cout << "ラッキーナンバー = " << lucky << std::endl;
+
     // 倍精度浮動小数
     double x, y;
     std::cout << "x:";
@@ -18,5 +26,5 @@ int main()
 
     std::cout << "x=" << x << std::endl;
     std::cout << "y=" << y << std::endl;
-    std::cout << "x+y=" << x+y << std::endl;
+    std::cout << "x+y=" << x + y << std::endl;
 }
